242d4e72630ce6c26e4740cb9bdcf452590739ec
Xuming <jinxm.alucard@gmail.com>
2025-05-12 21:59:04 +0800

    feat: update

--- src/components/ArticleCard/index.tsx
+++
@@ -1,9 +0,0 @@
-const ArticleCard: React.FC = () => {
-    return (
-      <div>
-   ArticleCard 
-      </div>
-    );
-  };
-  
-  export default ArticleCard;
--- src/components/Home/Index.tsx
+++
@@ -1,5 +0,0 @@
-// Home.tsx
-function Home() {
-  return <h1>Home Page</h1>;
-}
-export default Home;
